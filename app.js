71c0b8180c250288780d84c43b4175110ad83096
calleighwinberg <calleighwinberg@gmail.com>
2024-03-17 15:14:48 -0700

    ADDED image mult middleware. Added env vars. make sure this isn't published to git

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+
+//if in development, inlcude env vars
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
