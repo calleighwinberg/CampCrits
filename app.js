d1945a842d6395f449dcbd18dea082a96cc4aa96
calleighwinberg <calleighwinberg@gmail.com>
2024-03-20 08:32:13 -0700

    change connection to final

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-//const dbURL = process.env.DB_URL
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
+//const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
